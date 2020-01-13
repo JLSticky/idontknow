22ba3325119277ec652b0fa8168e910911ce2754
JLSticky <59576332+JLSticky@users.noreply.github.com>
2020-01-13 15:08:16 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Collections.Generic;
+
+namespace idk
+{
+    class Program
+    {
+        public static object RequestConstants { get; private set; }
+
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                using (var request = new HttpRequestMessage(new HttpMethod("GET"), "https://dfapi.digitalfleet.com/api/v2/Truck"))
+                {
+                    request.Headers.TryAddWithoutValidation("Accept", "application/json");
+                    request.Headers.TryAddWithoutValidation("X-API-KEY", "2437EA0D-C3D4-4879-9C92-6B6C52E9DF86");
+
+                   
+                    var response = await httpClient.SendAsync(request);
+
+                    Console.WriteLine(response);
+                   // Console.WriteLine(await response.Content.ReadAsStringAsync());
+                    var x = new Apiresponse<truck>();
+                    var y = JsonSerializer.Deserialize<Apiresponse<truck>>(await response.Content.ReadAsStringAsync());
+
+                    string T = await response.Content.ReadAsStringAsync();
+                    var options = new JsonSerializerOptions() { PropertyNameCaseInsensitive = true, WriteIndented = true };
+                    Apiresponse<truck> result = JsonSerializer.Deserialize<Apiresponse<truck>>(T, options);
+                    string cool = JsonSerializer.Serialize(result, options);
+                    Console.WriteLine(cool);
+
+                }
+            }
+        }
+    }
+}
+    
+
---
+++ responsedata.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace idk
+{
+    public class truck
+    {
+        public int? TruckId { get; set; }
+        public string TruckName { get; set; }
+        public int? DriverUserId { get; set; }
+        public string Vin { get; set; }
+        public string FunctionalityTier { get; set; }
+        public int? FunctionalityTierId { get; set; }
+        public bool? Active { get; set; }
+        public bool? Deleted { get; set; }
+        public string EldName { get; set; }
+        public int? TruckClassId { get; set; }
+        public string TruckClassName { get; set; }
+        public int? ClientDispatchId { get; set; }
+        public int? ClientId { get; set; }
+        public string UserNotes { get; set; }
+        public int? GpsUpdateMinutes { get; set; }
+        public int? IsType { get; set; }
+        
+        public tag[] tags { get; set; }
+
+    }
+    public class Apiresponse<T>
+    {
+        public Metadata metadata { get; set; }
+        public T[] data { get; set; }
+
+
+    }
+
+
+    public class Metadata
+    {
+        public Pagination pagination {get; set;}
+        public string Sort { get; set; }
+    }
+    public class Pagination
+    {
+        public int? Page { get; set; }
+        public int? PerPage { get; set; }
+        public int? TotalPageCount { get; set; }
+        public int? TotalRecordCount { get; set; }
+    }
+    
+    public class tag
+    {
+        public int? TagId { get; set; }
+        public string Name { get; set; }
+        public int? TagGroupID { get; set; }
+    }
+}
+
+
